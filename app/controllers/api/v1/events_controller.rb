b14a258cb175d071d31da4137353b4093134a134
Ariel Herman <aeh.herman@gmail.com>
2017-12-13 12:00:15 -0500

    serializer still not working

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::EventsController < ApplicationController
   def index
-    events = Event.all
-    render json: events.to_json
+    @events = Event.all
+    render json: @events
   end
 
   def show
     event = Event.find_by(id: params[:id])
-    @comments = event.comments
-    render json: event.to_json, include: 'comments'
+    # @comments = event.comments
+    render json: event.to_json
   end
 end
