3aadb57c26b35ccd8b7d84a0842d4aac3255f656
Ariel Herman <aeh.herman@gmail.com>
2017-12-14 11:06:35 -0500

    allow all origins, setup auth#create

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
       resources :designers, only: [:index]
       resources :events, only: [:index, :create, :show]
       resources :comments, only: [:index, :create, :update, :destroy]
-
+      post '/login', to: 'auth#create'
+      get '/current_user', to: 'auth#show'
     end
   end
 end
