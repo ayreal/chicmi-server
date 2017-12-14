265cad090ca1461ff4669912fde556b412953747
Ariel Herman <aeh.herman@gmail.com>
2017-12-14 13:24:25 -0500

    render specific user data

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,10 +6,37 @@ class Api::V1::AuthController < ApplicationController
        # issue user a token
       payload = {user_id: user.id}
       token = JWT.encode(payload, ENV["MY_SECRET"], ENV["KENNITH"])
-      render json: { token: token , currentUser: user}
+      render json: {
+        token: token,
+        id: user.id
+        name: user.name,
+        email: user.email,
+        photo: user.photo,
+        city_id: user.city_id,
+        username: user.username,
+        twitter: user.twitter,
+        instagram: user.instagram,
+        bio: user.bio
+       }
+
+
+      # create_table "users", force: :cascade do |t|
+      #   t.string "name"
+      #   t.string "email"
+      #   t.string "photo"
+      #   t.integer "city_id"
+      #   t.string "username"
+      #   t.string "twitter"
+      #   t.string "instagram"
+      #   t.text "bio"
+      #   t.string "password_digest"
+      #   t.datetime "created_at", null: false
+      #   t.datetime "updated_at", null: false
+      # end
+      #
     else
       render json: {error: "Could not authorize this user"}, status: 401
     end
   end
-  
+
 end
