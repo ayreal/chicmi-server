1f3825cee59d58db33e93983a760f0a2cf1de70d
Ariel Herman <aeh.herman@gmail.com>
2017-12-14 11:39:04 -0500

    issue JWT token in auth#create

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) do |repo_name|
 end
 
 gem 'active_model_serializers', '~> 0.10.0'
+gem 'jwt'
+gem 'figaro'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::AuthController < ApplicationController
 
   def create
-    byebug
+    user = User.find_by(username: params[:auth][:username])
+    if user && user.authenticate(params[:auth][:password])
+       # issue user a token
+      payload = {user_id: user.id}
+      token = JWT.encode(payload, ENV["MY_SECRET"], ENV["KENNITH"])
+      render json: { token: token , currentUser: user}
+    else
+      render json: {error: "Could not authorize this user"}, status: 401
+    end
   end
+  
 end
