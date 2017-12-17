59bf5b2d08300a0160c4014627c6544b73ac3b2c
Ariel Herman <aeh.herman@gmail.com>
2017-12-16 23:57:48 -0500

    event#create working

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -9,4 +9,21 @@ class Api::V1::EventsController < ApplicationController
     # @comments = event.comments
     render json: event.to_json
   end
+
+  def create
+    user = User.find_by(id: params[:user_id])
+    event = Event.find_by(external_id: params[:event][:event_id])
+    if !event
+      event = Event.new(event_params)
+      event.external_id = params[:event][:event_id]
+      event.save!
+    end
+    user.events << event
+    render json: event.to_json
+  end
+
+  private
+   def event_params
+     params.require(:event).permit(:external_id, :address_business_name, :address_street_1, :address_street_2, :slug, :address_city, :address_zip, :event_name_en, :start_date, :end_date, :summary, :is_hot, :event_hero_url)
+   end
 end
