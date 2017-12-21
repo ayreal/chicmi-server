e25908b2e48e443f6d8b545156fdb46607f26992
Ariel Herman <aeh.herman@gmail.com>
2017-12-20 20:59:53 -0500

    add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, uniqueness: true
+  validates :name, presence: true
+
   has_many :user_events
   has_many :events, through: :user_events
 
   has_many :comments
-  
+
   has_many :user_designers
   has_many :designers, through: :user_designers
 
