60fbb3862bcbfb137adbd57e04d0416ce944f605
Ariel Herman <aeh.herman@gmail.com>
2018-01-07 01:18:39 -0500

    validate username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password validations: false
   validates :username, uniqueness: true
+  validates :username, presence: true
   validates :name, presence: true
 
   has_many :user_events
